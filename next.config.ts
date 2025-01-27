fe862b66af0d135551db9134f9cffb7d5a5f171b
maorethians <apaladian@gmail.com>
2025-01-27 12:38:21 -0500

    feat: add github action

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const repo = 'portfolio';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export',
+    assetPrefix: `/${repo}/`,
+    basePath: `/${repo}`,
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
