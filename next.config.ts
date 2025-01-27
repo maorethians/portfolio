6af59b61bfac01c24a861f327513b268313d40aa
maorethians <apaladian@gmail.com>
2025-01-27 13:03:24 -0500

    fix: base url

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-const repo = 'portfolio';
-
 const nextConfig: NextConfig = {
     output: 'export',
-    assetPrefix: `/${repo}/`,
-    basePath: `/${repo}`,
     images: {
         unoptimized: true,
     },
